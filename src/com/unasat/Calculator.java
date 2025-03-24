fc233461b80e7102082d990a3493087b80ea0427
Sapnatic <ramharakhsapna@gmail.com>
2025-03-24 16:04:46 -0300

    eerste git upload

---
+++ src/com/unasat/Calculator.java
@@ -0,0 +1,26 @@
+package com.unasat;
+import java.util.Scanner; // 1. Scanner geimporteerd
+
+public class Calculator {
+    public static void main(String args[]) {
+
+Scanner sc = new Scanner(System.in); //2. Object gemaakt van die scanner.
+
+        System.out.println("Wat zou je willen doen?");
+        System.out.println("1.Optellen");
+        System.out.println("2.Aftrekken");
+        System.out.println("3.Vermenigvuldigen");
+        System.out.println("4.Delen");
+        System.out.println("");
+
+int userinput = sc.nextInt(); //3. User input opgeslagen in de variabel "userinpu"
+
+
+
+    }
+
+}
+
+
+
+
